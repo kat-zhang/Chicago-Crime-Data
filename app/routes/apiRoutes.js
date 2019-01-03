3e0c04208b6cb3261675e39ce3e1eb15500ea06e
Andrea Minhas <andrea.minhas@gmail.com>
2019-01-03 11:15:20 -0600

    api route with data is now working

---
+++ app/routes/apiRoutes.js
@@ -0,0 +1,13 @@
+var db = require("../models");
+
+module.exports = function(app) {
+  app.get("/api/crimes", function(req, res) {
+    console.log("We have our route");
+
+    db.Crime.findOne({
+      where: { Crime_ID: "10000092" }
+    }).then(function(dbCrimes) {
+      res.json(dbCrimes);
+    });
+  });
+};
