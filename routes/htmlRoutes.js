6de8c87904d659fa0eec348948a4dc8f5313afcb
Bharti <tagra.bharti@gmail.com>
2019-01-04 11:22:56 -0600

    added new files

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -5,4 +5,7 @@ module.exports = function (app) {
     app.get("/form", function (req, res) {
         res.render("form");
     });
+    app.get("/map", function (req, res) {
+        res.render("map");
+    });
 }
