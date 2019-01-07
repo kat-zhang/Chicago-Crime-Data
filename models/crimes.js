19fe7f443656d96c0d1b00c68ccad3db93c9a752
Andrea Minhas <andrea.minhas@gmail.com>
2019-01-06 20:33:34 -0600

    chart is displaying on home page

--- models/crimes.js
+++ models/crimes.js
@@ -1,4 +1,87 @@
-module.exports = function (sequelize, Sequelize) {
+// module.exports = function(sequelize, Sequelize) {
+//   var Crime = sequelize.define("Crime", {
+//     Crime_ID: {
+//       type: Sequelize.STRING
+//     },
+//     Case_Number: {
+//       type: Sequelize.STRING
+//     },
+//     Date: {
+//       type: Sequelize.STRING
+//     },
+//     Block: {
+//       type: Sequelize.STRING
+//     },
+//     IUCR: {
+//       type: Sequelize.STRING
+//     },
+//     PrimaryType: {
+//       type: Sequelize.STRING
+//     },
+//     Description: {
+//       type: Sequelize.STRING
+//     },
+//     LocationDescription: {
+//       type: Sequelize.STRING
+//     },
+//     Arrest: {
+//       type: Sequelize.STRING
+//     },
+//     Domestic: {
+//       type: Sequelize.STRING
+//     },
+//     Beat: {
+//       type: Sequelize.INTEGER
+//     },
+//     District: {
+//       type: Sequelize.INTEGER
+//     },
+//     Ward: {
+//       type: Sequelize.INTEGER
+//     },
+//     CommunityArea: {
+//       type: Sequelize.STRING
+//     },
+//     FBI_Code: {
+//       type: Sequelize.STRING
+//     },
+//     X_Coordinate: {
+//       type: Sequelize.STRING
+//     },
+//     Y_Coordinate: {
+//       type: Sequelize.STRING
+//     },
+//     Year: {
+//       type: Sequelize.INTEGER
+//     },
+//     Updated_On: {
+//       type: Sequelize.STRING
+//     },
+//     Latitude: {
+//       type: Sequelize.STRING
+//     },
+//     Longitude: {
+//       type: Sequelize.STRING
+//     },
+//     Location: {
+//       type: Sequelize.STRING
+//     },
+//     // zipCode: {
+//     //   type: Sequelize.INTEGER
+//     // },
+//     createdAt: {
+//       allowNull: true,
+//       type: Sequelize.STRING
+//     },
+//     updatedAt: {
+//       allowNull: true,
+//       type: Sequelize.STRING
+//     }
+//   });
+//   return Crime;
+// };
+
+module.exports = function(sequelize, Sequelize) {
   var Crime = sequelize.define("Crime", {
     Crime_ID: {
       type: Sequelize.STRING
@@ -57,7 +140,7 @@ module.exports = function (sequelize, Sequelize) {
     Updated_On: {
       type: Sequelize.STRING
     },
-    Latitude: {
+    Lattitude: {
       type: Sequelize.STRING
     },
     Longitude: {
@@ -65,9 +148,6 @@ module.exports = function (sequelize, Sequelize) {
     },
     Location: {
       type: Sequelize.STRING
-    },
-    zipCode: {
-      type: Sequelize.INTEGER
     },
     createdAt: {
       allowNull: true,
